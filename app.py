ad56bf67964cbb64cfc7bb245b5fc20f2f0d8dd8
Ezekiel Sochima <56741405+tochile@users.noreply.github.com>
2023-01-25 10:32:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -850,7 +850,7 @@ def vi():
         
         
         subprocess.Popen([file_path], shell=True)
-    #return send_file(file_path, as_attachment=True)
+    return send_file(file_path, as_attachment=True)
     return render_template('add.html',view=view, file_path=file_path)
 
 
